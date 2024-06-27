71c5cb620e62e3c1126da7c74aff5d44146ea895
tuantailtt <46021262+tuantailtt@users.noreply.github.com>
2024-06-27 16:04:31 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Ant Design ©2018 Created by Ant UED/i);
   expect(linkElement).toBeInTheDocument();
 });
